fd5d8769ce2862b67aac10bf65047566fd028bf8
grace09thompson <gracet2@myuw.net>
2015-05-13 16:32:30 -0700

    Initial Commit

---
+++ Cooks Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cooks Converter
+//
+//  Created by Grace Thompson on 5/13/15.
+//  Copyright (c) 2015 Grace Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
